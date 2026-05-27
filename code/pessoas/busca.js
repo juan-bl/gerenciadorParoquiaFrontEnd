4a7433cdbfd3e2f12ce7513f2de0385a512cc95e
Juan Nícolas <juannicolasdev@gmail.com>
2026-05-27 16:45:00 -0300

    feat: busca por pessoas

--- code/pessoas/busca.js
+++ code/pessoas/busca.js
@@ -3,3 +3,21 @@ const btnMostrarTodos = document.getElementById('btn-mostrar-todos');
 btnMostrarTodos.addEventListener('click', () => {
     renderizarPessoas();
 });
+
+
+const inputBusca = document.getElementById('input-busca');
+
+inputBusca.addEventListener('input', () => {
+    const texto = inputBusca.value.toLowerCase();
+
+    const filtradas = pessoas.filter(pessoa => pessoa.nome.toLowerCase().includes(texto));
+
+    renderizarPessoas(filtradas)
+})
+
+const btnClean = document.getElementById('claen-busca');
+
+btnClean.addEventListener('click', () => {
+    inputBusca.value = '';
+    renderizarPessoas();
+})
